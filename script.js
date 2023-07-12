7b59637f288ab0042f02ae43fb414490b5afd821
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-12 16:47:33 +0200

    addudateSelectedCount func

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 const container = document.querySelector(".container")
 const seats = document.querySelectorAll(".row .seat:not(.occupied)")
-console.log(seats)
+
 
 
 const count = document.getElementById("count")
@@ -12,14 +12,22 @@ const total = document.getElementById("total")
 const movieSelect = document.getElementById("movie")
 const ticketPrice = parseInt(movieSelect.value)
 
+//Update total and count 
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+    console.log("This",selectedSeats)
+}
+
+
+
 container.addEventListener("click",(e)=>{
-    console.log(e.target)
+    
     
     if(e.target.classList.contains("seat") 
     && !e.target.classList.contains("occupied")
     ){
       e.target.classList.toggle("selected");
-    
+    updateSelectedCount()
     }
   
 })
