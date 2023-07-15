ec14dc9668456f1821d195501d881d17cfe1c619
Mate Kamaras programming <matekamarasprogramming@mates-air.localdomain>
2023-07-15 13:46:56 +0200

    moved function call and some corrections

--- script.js
+++ script.js
@@ -1,108 +1,104 @@
 
 const container = document.querySelector(".container")
 const seats = document.querySelectorAll(".row .seat:not(.occupied)")
-const allSeats = document.querySelectorAll(".row .seat")
 const movieSelect = document.getElementById("movie")
-
-console.log(typeof seats)
+const count = document.getElementById("count")
+const total = document.getElementById("total")
 
 populateUI();
 
-const count = document.getElementById("count")
-const total = document.getElementById("total")
 let ticketPrice = parseInt(movieSelect.value)
 
 
 //save selected movie and preise
 
 function setMovieData(movieIndex,moviePrice){
-
+  
   localStorage.setItem("selectedMovieIndex",movieIndex)
   localStorage.setItem("selectedMoviePrice",moviePrice)
 }
 
-movieSelect.sele
-
 //Update total and count 
 function updateSelectedCount(){
-    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
   
-
-    //copy selected seats into arr 
-    const seatsIndex = [...selectedSeats].map((seat)  => [...seats].indexOf(seat));
-
-    //saving to local storage
-    localStorage.setItem("selectedSeats",JSON.stringify(seatsIndex))
-   
-
   
-    //Map through array 
-    //return a new array indexe
-
+  //copy selected seats into arr 
+  const seatsIndex = [...selectedSeats].map((seat)  => [...seats].indexOf(seat));
   
-
+  //saving to local storage
+  localStorage.setItem("selectedSeats",JSON.stringify(seatsIndex))
   
-
-
-
-
-    const selectedSeatsCount = selectedSeats.length
-
-
-    count.innerHTML = selectedSeatsCount;
-    total.innerHTML =selectedSeatsCount* ticketPrice
-
+  
+  
+  //Map through array 
+  //return a new array indexe
+  const selectedSeatsCount = selectedSeats.length
+  
+  
+  count.innerHTML = selectedSeatsCount;
+  total.innerHTML =selectedSeatsCount* ticketPrice
+  
 }
 
 //Get data from localstorage and populate UI
 
 function populateUI() {
   const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
-   console.log(selectedSeats.length)
-
-   //check if selectedSeats arr has elements
-
-  if(selectedSeats.length>-0 && selectedSeats !== null ){
-    console.log(selectedSeats)
-    selectedSeats.forEach((seat,index)=>{
+  console.log("1",selectedSeats.length)
+  
+  //check if selectedSeats arr has elements
+  
+  if(selectedSeats.length>0 && selectedSeats !== null ){
+    console.log("?",selectedSeats)
+    seats.forEach((seat,index)=>{
+      console.log("seat",seat)
      
-     const storedSeat = seats[seat]
-    
-     
-     if(selectedSeats.indexOf(index)>-1){
-       storedSeat.classList.add("selected")
-       
-    }
-    
+      //const storedSeat = seats[seat]
+
+      //console.log("storedSeat",storedSeat)
+      console.log("seat",selectedSeats)
+      
+      
+      if(selectedSeats.indexOf(index)>-1){
+        seat.classList.add("selected")
+        
+      }
+      
     })
   }
   const selectedMovieIndex = localStorage.getItem("selectedMovie")
+
   if(selectedMovieIndex !==null){
-    movieSelect.selectedIndex = selectedMovieIndex
+    movieSelect.selectedIndex = selectedMovieIndex   
   }
 }
 
 // Movie select event
 movieSelect.addEventListener("change",(e)=>{
   ticketPrice = parseInt(movieSelect.value)
- setMovieData(e.target.selectedIndex,e.target.value);
-
+  setMovieData(e.target.selectedIndex,e.target.value);
+  
   updateSelectedCount()
 })
 
 // Seat click event 
 
 container.addEventListener("click",(e)=>{
-    
-    
-    if(e.target.classList.contains("seat") 
-    && !e.target.classList.contains("occupied")
-    ){
-      e.target.classList.toggle("selected");
-      updateSelectedCount()
-    }
+  
+  
+  if(e.target.classList.contains("seat") 
+  && !e.target.classList.contains("occupied")
+  ){
+    e.target.classList.toggle("selected");
+    updateSelectedCount()
+  }
   
 })
+
+// initial count and total set
+
+updateSelectedCount()
 
 
 
